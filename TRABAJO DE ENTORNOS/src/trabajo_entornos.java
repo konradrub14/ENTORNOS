ab6c9c442142850a0fe6bfc8d5dc40116c1032c5
konradrub14 <73341471+konradrub14@users.noreply.github.com>
2020-11-11 09:02:25 +0100

    Update trabajo_entornos.java
    PUNTO 6 TRABAJO ENTORNOS

--- TRABAJO DE ENTORNOS/src/trabajo_entornos.java
+++ TRABAJO DE ENTORNOS/src/trabajo_entornos.java
@@ -13,10 +13,11 @@ public class trabajo_entornos {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//EJERCICIO DE PROGRAMACÓN, SUBO EL ARCHIVO A GITHUB
+		//EJERCICIO DE PROGRAMACÃ“N, SUBO EL ARCHIVO A GITHUB
 		
 		
 	System.out.println("Realizo un cambio en el archivo subido  github");
+		System.out.println("EJERCICIO 6: VAMOS A REALIZAR UN CAMBIO EN EL CODIGO Y LO DESCARGAREMOS AL ECLIPSE");
 	
 	}
 
