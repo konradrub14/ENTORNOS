f86f6ab2f5bef86d98df57305a7d03eb7b0431be
konrad <konradrub14@gmail.com>
2021-02-18 18:41:06 +0100

    TRABAJO ENTORNOS

--- ENTORNOS_TEMA4_/src/COMPLEX/ComplexTest.java
+++ ENTORNOS_TEMA4_/src/COMPLEX/ComplexTest.java
@@ -8,21 +8,21 @@ class ComplexTest {
 
 	@Test
 	void testSumar() {
-		Complex prueba= new Complex(50,30);
-		Complex prueba2= new Complex(20,5);
+		Complex numero1= new Complex(50,30);
+		Complex numero2= new Complex(20,5);
 		
-		Complex resultado=prueba.sumar(prueba2);
+		Complex resultado=numero1.sumar(numero2);
 		Complex resultado_esperado= new Complex(70,35);
 		assertEquals(resultado,resultado_esperado);
 	}
 
 	@Test
 	void testRestar() {
-		Complex prueba3= new Complex(100,40);
-		Complex prueba4= new Complex(20,15);
-		Complex resultado1=prueba3.restar(prueba4);
-		Complex resultado_esperado1= new Complex(80,25);
-		assertEquals(resultado1,resultado_esperado1);
+		Complex numero1= new Complex(100,40);
+		Complex numero2= new Complex(20,15);
+		Complex resultado=numero1.restar(numero2);
+		Complex resultado_esperado= new Complex(80,25);
+		assertEquals(resultado,resultado_esperado);
 	}
 
 	@Test
@@ -64,6 +64,40 @@ class ComplexTest {
 		Complex numero1= new Complex(5,8);
 		Complex numero2= new Complex(5,8);
 		Complex numero3= new Complex(3.5,6.7);
+	}
+	// RESTA Y DIVISIÓN
+	@Test
+	void testDividir1() {
+		Complex numero1=new Complex(3.12,2.14);
+		Complex numero2=new Complex(1.1,1.3);
+		Complex resultado= numero1.dividir(numero2);
+		Complex resultado_esperado=new Complex(2.142758620689655, -0.5868965517241377);
+		assertEquals(resultado,resultado_esperado);
+		
+	}
+	@Test
+	void testDividir2() {
+		Complex numero1=new Complex(-2.1,1.5);
+		Complex numero2=new Complex(1.32,-1.98);
+		Complex resultado= numero1.dividir(numero2);
+		Complex resultado_esperado= new Complex(-1.013986013986014, -0.3846153846153847);
+		assertEquals(resultado,resultado_esperado);
+	}
+	@Test
+	void testRestar1() {
+		Complex numero1= new Complex(-2,1);
+		Complex numero2= new Complex(-2,1.2);
+		Complex resultado=numero1.restar(numero2);
+		Complex resultado_esperado= new Complex(0, -0.19999999999999996);
+		assertEquals(resultado,resultado_esperado);
+	}
+	@Test
+	void testRestar2() {
+		Complex numero1= new Complex(1,-5);
+		Complex numero2= new Complex(2,-5);
+		Complex resultado=numero1.restar(numero2);
+		Complex resultado_esperado= new Complex(-1,0);
+		assertEquals(resultado,resultado_esperado);
 	}
 
 }
